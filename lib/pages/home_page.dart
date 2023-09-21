c18c396621600f374d0360f433703884cc98c263
omarx33 <carlosmorih33@gmail.com>
2023-09-21 02:34:40 -0500

    esta version es funcional conectada a un nuevo firebase, realizando cruds de prueba

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   //apuntando a la bd bd_task de firebase
   CollectionReference bd_taskReference =
-      FirebaseFirestore.instance.collection('bd_task');
+      FirebaseFirestore.instance.collection('bd_prueba');
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +19,90 @@ class HomePage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                bd_taskReference.get().then((value) {
-                  print(value.docs);
+                bd_taskReference.get().then((QuerySnapshot value) {
+                  // QuerySnapshot coleccion = value;
+                  // List<QueryDocumentSnapshot> docs = coleccion.docs;
+                  // QueryDocumentSnapshot doc = docs[0];
+                  // print(doc.id);
+                  // print(doc.data());
+
+                  QuerySnapshot coleccion = value;
+                  coleccion.docs.forEach((QueryDocumentSnapshot element) {
+                    Map<String, dynamic> myMap =
+                        element.data() as Map<String, dynamic>;
+                    print(myMap["titulo"]);
+                  });
                 });
-                //  print(bd_taskReference.id);
+                // print(bd_taskReference.id);
               },
               child: Text("Obtener dato"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                bd_taskReference.add(
+                  {
+                    "titulo": "ir a comer segunda prueba",
+                    "descripcion": "comprar",
+                  },
+                ).then((DocumentReference value) {
+                  print(value.id);
+                }).catchError((error) {
+                  print("error de registro");
+                }).whenComplete(() {
+                  print("registrado");
+                });
+              },
+              child: Text("Agregar Documento"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                bd_taskReference.doc("SkooDip7JdjeoUZr604k").update(
+                  {
+                    "titulo": "Ir de paseo 2",
+                    //   "descripcion": "salir temprano", <-- si no se envia, la bd lo deja tal cual
+                  },
+                ).catchError(
+                  (error) {
+                    print(error);
+                  },
+                ).whenComplete(
+                  () {
+                    print("Actualizado");
+                  },
+                );
+              },
+              child: Text("Actualizar documento"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                bd_taskReference
+                    .doc("xgzfInjDsC7ayZuHqSVn")
+                    .delete()
+                    .catchError(
+                  (error) {
+                    print(error);
+                  },
+                ).whenComplete(() {
+                  print("Eliminado");
+                });
+              },
+              child: Text("Eliminar"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //id personalizado
+                bd_taskReference.doc("123456").set(
+                  {
+                    "titulo": "ir al concierto",
+                    "descripcion": "el fin de semana",
+                  },
+                ).catchError((error) {
+                  print(error);
+                }).whenComplete(() {
+                  print("Creación completa");
+                });
+              },
+              child: Text("Agregar Documento personalizado"),
             ),
           ],
         ),
